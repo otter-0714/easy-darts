5d6a56434b40b553fdfe92c47cbcc15680552928
masato <mm140701mm@gmail.com>
2024-08-30 18:06:47 +0900

    [fix] change prisma log

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient({
-  log: ["error", "warn"],
+  log: ["query", "error", "info", "warn"],
 });
 export default prisma;
 
